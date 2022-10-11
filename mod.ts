2e4d0022beafb6da54571c04a317bb7274e0ac40
rosnerdev <yrosner123@gmail.com>
2022-10-11 09:21:11 +0300

    changed mod.ts location

---
+++ mod.ts
@@ -0,0 +1,2 @@
+export { Anime, Character, Manga } from './src/lib/index.ts'
+export * from './src/typings/index.ts'
--- src/mod.ts
+++
@@ -1,2 +0,0 @@
-export { Anime, Character, Manga } from './lib/index.ts'
-export * from './typings/index.ts'
